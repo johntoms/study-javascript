25db7e3dab3dd6c5cbb669f3852b4b11062789a5
johntoms <17621672714@163.com>
2019-03-11 20:27:18 +0800

    完善 学习实例 函数

--- 学习实例/引用外部 js 实例/myScript.js
+++ 学习实例/引用外部 js 实例/myScript.js
@@ -8,3 +8,17 @@ function numberAdd() {
     z = x + y;
     document.getElementById("demo1").innerHTML = z;
 }
+
+function displayName() {
+    let person = {
+        FirstName: "John",
+        LastName: "Toms",
+        fullName: function () {
+            return this.FirstName + " " + this.LastName;
+        }
+    };
+    document.getElementById("name1").innerHTML = '加括号输出函数执行结果：' + person.fullName();
+    document.getElementById("name2").innerHTML = '不加括号输出函数表达式：' + person.fullName;
+}
+
+
